bce5dbb9e4112262b227895e10e6f3c69d7fad99
iveswang <1660583890@qq.com>
2025-04-14 23:05:16 +0800

    feat: plugin deinit

--- src/TemplatePluginCall.cpp
+++ src/TemplatePluginCall.cpp
@@ -5,12 +5,12 @@
 #include <spdlog/async.h>
 
 #include <TemplatePluginCall.h>
+#include <LoggerRegisterHelpper.h>
 
 #include "TemplatePlugin.h"
 #include "TemplatePluginLog.h"
 
 TemplatePlugin* pPlugin = nullptr;
-constexpr int logFileMaxSize = 100 * 1024 * 1024;  // 20M
 
 void initDir(const char* dir)
 {
@@ -19,7 +19,7 @@ void initDir(const char* dir)
     {
         if (!spdlog::get(templatePluginName))
         {
-            spdlog::rotating_logger_mt<spdlog::async_factory>(templatePluginName, std::string(dir) + "log/" + templatePluginName + ".log", logFileMaxSize, 100);
+            LoggerRegisterHelpper::registerLogger(templatePluginName, std::string(dir) + "log/" + templatePluginName + ".log");
         }
     }
     catch (const std::exception& e)
@@ -44,4 +44,9 @@ void pluginDeinit()
         delete pPlugin;
         pPlugin = nullptr;
     }
+}
+
+void deinit()
+{
+    LoggerRegisterHelpper::unregisterLogger(templatePluginName);
 }
